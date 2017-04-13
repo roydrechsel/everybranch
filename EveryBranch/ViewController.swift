5b18551fe6579c5e1475cea6942f4276fa0ce19d
Andrew Drechsel <roydrechsel@gmail.com>
2017-04-13 16:33:05 -0600

    Initial Commit

---
+++ EveryBranch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EveryBranch
+//
+//  Created by Andrew Drechsel on 4/13/17.
+//  Copyright © 2017 Andrew Drechsel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
